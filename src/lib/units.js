fbff9272d5a8a1c18d0ca9942abb405edebf16a3
Wiar8 <master@wiar8.com>
2022-10-04 20:53:59 -0400

    Add units formatter

---
+++ src/lib/units.js
@@ -0,0 +1,26 @@
+const units = (num) => {
+  switch (num) {
+    case 1:
+      return "UNO";
+    case 2:
+      return "DOS";
+    case 3:
+      return "TRES";
+    case 4:
+      return "CUATRO";
+    case 5:
+      return "CINCO";
+    case 6:
+      return "SEIS";
+    case 7:
+      return "SIETE";
+    case 8:
+      return "OCHO";
+    case 9:
+      return "NUEVE";
+  }
+
+  return "";
+};
+
+export { units };
